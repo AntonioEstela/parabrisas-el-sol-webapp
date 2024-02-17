11644532eb5fb60432437f31555fdf9cfa0b0715
Antonio Estela <antonioestela73@gmail.com>
2024-02-17 17:09:00 -0500

    feat: add images and refactor code

--- src/app/components/Servicios/Servicios.tsx
+++ src/app/components/Servicios/Servicios.tsx
@@ -1,50 +1,11 @@
 import ServicioCard from './ServicioCard/ServicioCard';
-import { ServicioCardProps } from '../../types';
-
-const servicios: Array<ServicioCardProps> = [
-  {
-    description:
-      'Reparamos fisuras (quebraduras) en parabrisas y vidrios laminados. Ahorra dinero y tiempo. Reparemos tu parabrisas dañado en lugar de reemplazarlo.',
-    imageUrl: '/reparacion-de-fisura.jpg',
-    title: 'Reparación de fisuras',
-  },
-  {
-    description:
-      'Eliminamos rayones y desperfectos de forma profesional, mejorando la visibilidad, seguridad y valor de tu propiedad.',
-    imageUrl: '/reparacion-de-fisura.jpg',
-    title: 'Reparación de rayones en vidrios',
-  },
-  {
-    description:
-      'Fabricación de vidrios para todo tipo de vehículos. Calidad profesional, óptimos materiales y atención personalizada. Instalaciones aptas para cualquier tipo de vehículo.',
-    imageUrl: '/reparacion-de-fisura.jpg',
-    title: 'Vidrios a la medida',
-  },
-  {
-    description:
-      '¡Experimenta la potencia del polarizado nanocerámico! Reduce el calor 5 veces más que los convencionales, filtra el 90% de los rayos UV, ¡sin cambiar la apariencia interior del vidrio! ¡Deslumbra con eficiencia!',
-    imageUrl: '/reparacion-de-fisura.jpg',
-    title: 'Polarizado Nanocerámico',
-  },
-  {
-    description:
-      'Transformamos tu vehículo con nuestro servicio de Detailing y embellecimiento. Devolvemos el brillo original, cuidamos cada detalle y dejamos tu auto impecable. Descubre el arte de la belleza automotriz con nosotros.',
-    imageUrl: '/reparacion-de-fisura.jpg',
-    title: 'Detailing',
-  },
-  {
-    description:
-      'Garantizamos la excelencia en el mantenimiento de vidrios y puertas para vehículos blindados. Confía en nosotros para mantener tu seguridad en óptimas condiciones.',
-    imageUrl: '/reparacion-de-fisura.jpg',
-    title: 'Blindados: Mantenimiento de Vidrios y Puertas',
-  },
-];
+import { servicios } from '@/app/constants';
 
 export default function Servicios() {
   return (
-    <section className=''>
-      <h2 className='mb-4 text-2xl font-bold text-center text-own-yellow md:text-4xl'>Servicios</h2>
-      <div className='grid grid-cols-1 gap-6 justify-items-center md:grid-cols-2 md:gap-14'>
+    <section className='flex flex-col items-center'>
+      <h2 className='my-10 text-3xl font-bold text-center md:my-16 text-own-yellow md:text-5xl'>Servicios</h2>
+      <div className='grid grid-cols-1 gap-6 md:w-4/5 justify-items-center md:grid-cols-3 md:gap-14'>
         {servicios.map(({ description, imageUrl, title }, index) => (
           <ServicioCard description={description} imageUrl={imageUrl} title={title} key={index} />
         ))}
---
+++ src/app/constants.ts
@@ -0,0 +1,40 @@
+import { ServicioCardProps } from './types';
+
+export const servicios: Array<ServicioCardProps> = [
+  {
+    description:
+      'Reparamos fisuras (quebraduras) en parabrisas y vidrios laminados. Ahorra dinero y tiempo. Reparemos tu parabrisas dañado en lugar de reemplazarlo.',
+    imageUrl: '/reparacion-de-fisura.jpg',
+    title: 'Reparación de fisuras',
+  },
+  {
+    description:
+      'Eliminamos rayones y desperfectos de forma profesional, mejorando la visibilidad, seguridad y valor de tu propiedad.',
+    imageUrl: '/reparacion-fisura.jpg',
+    title: 'Reparación de rayones en vidrios',
+  },
+  {
+    description:
+      'Fabricación de vidrios para todo tipo de vehículos. Calidad profesional, óptimos materiales y atención personalizada. Instalaciones aptas para cualquier tipo de vehículo.',
+    imageUrl: '/fabricacion-vidrios.jpeg',
+    title: 'Vidrios a la medida',
+  },
+  {
+    description:
+      '¡Experimenta la potencia del polarizado nanocerámico! Reduce el calor 5 veces más que los convencionales, filtra el 90% de los rayos UV, ¡sin cambiar la apariencia interior del vidrio! ¡Deslumbra con eficiencia!',
+    imageUrl: '/resultado-polarizado.jpeg',
+    title: 'Polarizado Nanocerámico',
+  },
+  {
+    description:
+      'Transformamos tu vehículo con nuestro servicio de Detailing y embellecimiento. Devolvemos el brillo original, cuidamos cada detalle y dejamos tu auto impecable. Descubre el arte de la belleza automotriz con nosotros.',
+    imageUrl: '/proceso-detailing.jpeg',
+    title: 'Detailing',
+  },
+  {
+    description:
+      'Garantizamos la excelencia en el mantenimiento de vidrios y puertas para vehículos blindados. Confía en nosotros para mantener tu seguridad en óptimas condiciones.',
+    imageUrl: '/trabajo-parabrisas-blindado.jpeg',
+    title: 'Blindados: Mantenimiento de Vidrios y Puertas',
+  },
+];
